30f7b33f5e44be7283a6aa62fa52fb3eb011e3b3
GitHub Action <action@github.com>
2020-07-01 02:12:50 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jooyeonsong/hellointellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jooyeonsong.hellointellijplugin.services
+
+import com.github.jooyeonsong.hellointellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
